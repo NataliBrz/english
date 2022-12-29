0b799edc2caa2fc60f07a1d72f856e4ea0070a5e
NataliBrz <ash_kronius@mail.ru>
2022-12-29 21:58:11 +0100

    project

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+import './Card.css';
+
+function Card() {
+  return (
+    <div className="Card">
+      <div>murz</div>
+      <img src='https://www.wallpaperflare.com/static/642/294/549/cat-grass-leaves-autumn-wallpaper.jpg' />
+    </div>
+  );
+}
+
+export default Card;
